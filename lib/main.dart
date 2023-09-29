76e6032fe1d5f80ce265f482c6c9fb6f2451f18c
Eng.Ahmed Shabaan <ma2155151@gmail.com>
2023-09-30 02:38:48 +0300

    ubdate text

--- lib/main.dart
+++ lib/main.dart
@@ -10,14 +10,52 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text("Title"),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          width: double.infinity,
+          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 40),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 10,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(width: 3, color: Colors.green),
+                      borderRadius: BorderRadius.circular(50.0)),
+                  hintText: "Email",
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(width: 3, color: Colors.green),
+                      borderRadius: BorderRadius.circular(50.0)),
+                  hintText: "Passowrd",
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+    ElevatedButton(
+    child: Text('Enter'),
+    style: ElevatedButton.styleFrom(
+    primary: Colors.green,
+    // side: BorderSide(color: Colors.yellow, width: 5),
+    textStyle: const TextStyle(
+    color: Colors.white, fontSize: 25, fontStyle: FontStyle.normal),
+    shape: BeveledRectangleBorder(
+    borderRadius: BorderRadius.all(Radius.circular(5))),
+    shadowColor: Colors.lightBlue), onPressed: () {  },
+    )   ],
           ),
-          body: Center(heightFactor: 15,
-            child:
-          Text("Body" ,style: TextStyle(fontSize: 20),),)
-        ));
+        ),
+      ),
+    );
   }
 }
